68558f203dc76e19c35b8d419bafa2a061cfe2a6
johnwest80 <github@johnwest.com>
2018-08-30 09:09:59 -0400

    add authorization header to cors

--- api/dist/src/server.js
+++ api/dist/src/server.js
@@ -18,7 +18,7 @@ db.once('open', () => {
     const port = (process.env.PORT || 3000);
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         next();
     });
     app.use(bodyParser.urlencoded({ extended: false }));
--- api/src/server.ts
+++ api/src/server.ts
@@ -20,7 +20,7 @@ db.once('open', () => {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   });
 
