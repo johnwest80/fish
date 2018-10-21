128e99394ee5b0bfbc4575d9a0a69386e08a61aa
johnwest80 <github@johnwest.com>
2018-10-21 13:33:37 -0400

    support me call build

--- api/dist/src/controllers/user.controller.js
+++ api/dist/src/controllers/user.controller.js
@@ -6,6 +6,11 @@ const router = express_1.Router();
 router.get('/me', AuthenticationService_1.AuthenticationService.verifyToken, (req, res, next) => {
     res.send({ username: req.user.username, email: req.user.email, name: req.user.name });
 });
+router.post('/me', AuthenticationService_1.AuthenticationService.verifyToken, (req, res, next) => {
+    req.user.name = req.body.name;
+    req.user.save();
+    res.send();
+});
 // Export the express.Router() instance to be used by server.ts
 exports.UserController = router;
 //# sourceMappingURL=user.controller.js.map
--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -15,5 +15,11 @@ router.get('/me', AuthenticationService.verifyToken, (req: IAuthenticatedRequest
     res.send({ username: req.user.username, email: req.user.email, name: req.user.name } as IMe);
 });
 
+router.post('/me', AuthenticationService.verifyToken, (req: IAuthenticatedRequest, res: Response, next: NextFunction) => {
+    req.user.name = req.body.name;
+    req.user.save();
+    res.send();
+});
+
 // Export the express.Router() instance to be used by server.ts
 export const UserController: Router = router;
