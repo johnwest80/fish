59428b65c285296f511c790b74de84f2f827f9f3
johnwest80 <github@johnwest.com>
2018-11-23 22:42:46 -0500

    build

--- api/dist/src/server.js
+++ api/dist/src/server.js
@@ -21,7 +21,7 @@ db.once('open', () => {
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
         next();
     });
     app.use(bodyParser.urlencoded({ extended: false }));
